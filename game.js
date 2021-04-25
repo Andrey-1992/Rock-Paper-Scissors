789b57065cb8174a70ee20deac005a917bb89a91
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-04-24 19:53:54 -0600

    Add instances of player

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-
+GAME.JS
+class Game {
+  constructor(player1, player2) {
+    this.human = new Player({name:"human", icon:"humanIcon", score: 0, chooseWeapon: ""});
+    this.computer = new Player({name:"computer", icon:"comptureIcon", score: 0, chooseWeapon: ""});
+  }
