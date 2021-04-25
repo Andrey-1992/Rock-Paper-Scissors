88537eb17f8ea69d4dab4542b906fd8dbd21ee70
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-04-24 20:07:31 -0600

    Correct indententions

--- game.js
+++ game.js
@@ -1,8 +1,7 @@
-GAME.JS
 class Game {
   constructor(player1, player2) {
-    this.human = new Player({name:"human", icon:"humanIcon", score: 0, chooseWeapon: ""});
-    this.computer = new Player({name:"computer", icon:"comptureIcon", score: 0, chooseWeapon: ""});
+    this.human = new Player({name:"human", icon:"humanIcon", score: 0, chooseWeapon: "paper"});
+    this.computer = new Player({name:"computer", icon:"comptureIcon", score: 0, chooseWeapon: "scissors"});
   }
   classicGame(player1, player2) {
     if (player1.chooseWeapon === player2.chooseWeapon) {
@@ -15,21 +14,22 @@ class Game {
       if (player2.chooseWeapon === "paper") {
         return `${player2.name} Won!`;
       }
-   }
-   if (player1.chooseWeapon === "scissors") {
-      if (player2.chooseWeapon === "paper") {
-         return `${player1.name} Won!`;
-     }
-      if (player2.chooseWeapon === "rock") {
-       return `${player2.name} Won!`;
-     }
-  }
-  if (player1.chooseWeapon === "paper") {
-      if (player2.chooseWeapon === "rock") {
-      return `${player1.name} Won!`;
     }
+    if (player1.chooseWeapon === "scissors") {
+      if (player2.chooseWeapon === "paper") {
+        return `${player1.name} Won!`;
+      }
+      if (player2.chooseWeapon === "rock") {
+        return `${player2.name} Won!`;
+      }
+    }
+    if (player1.chooseWeapon === "paper") {
+      if (player2.chooseWeapon === "rock") {
+        return `${player1.name} Won!`;
+      }
       if (player2.chooseWeapon === "scissors") {
-      return `${player2.name} Won!`;
+        return `${player2.name} Won!`;
+      }
     }
   }
 }
