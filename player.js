92a69a65c79c68a891b3744d4aa615433495f025
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-04-26 14:09:33 -0600

    Update Player class

--- player.js
+++ player.js
@@ -2,8 +2,13 @@ class Player {
   constructor(features) {
     this.name = features.name;
     this.token = features.icon;
-    this.wins = features.score;
-    this.chooseWeapon = features.weapon;
+    this.wins = 0;
+    this.weapon = "";
+    this.weapons = ["rock", "scissors", "paper"];
+  }
+  chooseRandomWeapon() {
+  var randomIndex = Math.floor(Math.random() * this.weapons.length);
+  this.weapon = this.weapons[randomIndex];
   }
   saveWinsToStorage() {
 
@@ -13,5 +18,11 @@ class Player {
   }
   takeTurn() {
 
+  }
+  updateScore() {
+   this.wins ++ ;
+  }
+  chooseWeapon(weapon) {
+    this.weapon = weapon;
   }
 }
